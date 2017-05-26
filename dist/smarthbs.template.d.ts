f917820545d03c6ead9538f5e942dc11bcd31ff3
Phil Kunz <gitlab@philkunz.com>
2017-05-26 15:16:33 +0200

    add getTemplateForString

--- dist/smarthbs.template.d.ts
+++ dist/smarthbs.template.d.ts
@@ -3,3 +3,7 @@
  * get a template for a file on disk
  */
 export declare let getTemplateForFile: (filePathArg: string) => Promise<HandlebarsTemplateDelegate>;
+/**
+ * get a template for string
+ */
+export declare let getTemplateForString: (fileStringArg: string) => Promise<HandlebarsTemplateDelegate>;
--- ts/smarthbs.template.ts
+++ ts/smarthbs.template.ts
@@ -7,3 +7,10 @@ export let getTemplateForFile = async (filePathArg: string) => {
   let filePathAbsolute = plugins.path.resolve(filePathArg)
   return plugins.handlebars.compile(plugins.smartfile.fs.toStringSync(filePathAbsolute))
 }
+
+/**
+ * get a template for string
+ */
+export let getTemplateForString = async (fileStringArg: string) => {
+  return plugins.handlebars.compile(fileStringArg)
+}
